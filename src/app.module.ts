708f10d53f0b959a654c8b8b9091566c817147a1
humberto.luis <humberto.luis@pontaltech.com.br>
2024-12-23 17:38:19 -0300

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UrlModule } from './url/url.module'; 
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, UrlModule],
+  imports: [UserModule, UrlModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
