03d2b0981977decb13732274f87c859f55e864c1
humberto.luis <humberto.luis@pontaltech.com.br>
2024-12-23 22:37:50 -0300

    feat: add validate users method on auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import { User } from 'src/user/entities/user.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
-    private jwtService: JwtService,
+    private jwtService: JwtService
   ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
@@ -27,5 +28,30 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async validateUserFromRequest(request): Promise<User> {
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token, {
+        secret: process.env.JWT_SECRET
+      });
+
+      const user = await this.userService.findOne(decoded.sub);
+
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      console.error("Error verifying token: ", error);
+      throw new UnauthorizedException('Invalid or expired token');
+    }
   }
 }
